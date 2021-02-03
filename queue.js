ccc51da749515c0f352813bd77825556aeabbf87
mansoorishaq1 <77729021+mansoorishaq1@users.noreply.github.com>
2021-02-03 21:05:57 +0500

    Update queue.js

--- queue.js
+++ queue.js
@@ -4,16 +4,16 @@ var myRealQueue=[];
 function addtoQueue(value){
     myQueue.push(value);
 }
-// function removeQueue(){
-//     for(let i=myQueue.length;i>0;i--){
-//         myRealQueue.push(myQueue.pop());
-//         console.log(myRealQueue);
-//     }
-//     myRealQueue.pop();
-//     for(let i=myRealQueue.length;i>0;i--){
-//         myQueue.push(myRealQueue.pop());
-//     }
-// }
+function removeQueue(){
+    for(let i=myQueue.length;i>0;i--){
+        myRealQueue.push(myQueue.pop());
+        console.log(myRealQueue);
+    }
+    myRealQueue.pop();
+    for(let i=myRealQueue.length;i>0;i--){
+        myQueue.push(myRealQueue.pop());
+    }
+}
         function removeQueue(){
             for(let i=0;i<=myQueue.length;i++){
                 myRealQueue.push(myQueue.pop());
